0c70ccde31b4821c6105ec5aadf8daaefe7fb78e
Sergio Sanchez <sergio.sanchez@canarddrones.com>
2018-12-07 14:05:40 +0100

    Initial commit. Added Tabs

---
+++ app/src/main/java/testingapps/serj/android_realm_test/AddEntryFragment.java
@@ -0,0 +1,14 @@
+package testingapps.serj.android_realm_test;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddEntryFragment extends Fragment {
+
+}
